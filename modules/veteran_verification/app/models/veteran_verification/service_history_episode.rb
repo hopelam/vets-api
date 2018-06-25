31847e4680ea9efb03d00be1436a77f0f0bb515c
Edward Paget <edward@cassetteta.pe>
2018-06-25 09:29:39 -0400

    Fix double quote to single quote

--- modules/veteran_verification/app/models/veteran_verification/service_history_episode.rb
+++ modules/veteran_verification/app/models/veteran_verification/service_history_episode.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require "active_support/core_ext/digest/uuid"
+require 'active_support/core_ext/digest/uuid'
 
 module VeteranVerification
   class ServiceHistoryEpisode
